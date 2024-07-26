374437aebe76a410f490af496cb774aded840e3d
chenshuo-vi <84225266+chenshuo-vi@users.noreply.github.com>
2024-07-27 01:41:55 +0800

    Update diy.sh

--- diy.sh
+++ diy.sh
@@ -12,11 +12,11 @@ sed -i 's/luci-theme-bootstrap/luci-theme-argon/g' feeds/luci/collections/luci/M
 
 #修改wifi信息
 #5g
-sed -i 's/Channel=36/Channel=44/g' package/emortal/mt-drivers/mt_wifi/files/mt7615.dat
-sed -i 's/SSID1=OpenWrt/SSID1=qqq_5G/g' package/emortal/mt-drivers/mt_wifi/files/mt7615.dat
-sed -i 's/AuthMode=OPEN/AuthMode=WPA2PSK/g' package/emortal/mt-drivers/mt_wifi/files/mt7615.dat
-sed -i '1,/EncrypType=NONE/s/EncrypType=NONE/EncrypType=AES/' package/emortal/mt-drivers/mt_wifi/files/mt7615.dat
-sed -i 's/WPAPSK1=12345678/WPAPSK1=987654321/g' package/emortal/mt-drivers/mt_wifi/files/mt7615.dat
+sed -i 's/Channel=36/Channel=44/g' package/emortal/mt-drivers/mt_wifi/files/mt7615.5G.dat
+sed -i 's/SSID1=OpenWrt/SSID1=qqq_5G/g' package/emortal/mt-drivers/mt_wifi/files/mt7615.5G.dat
+sed -i 's/AuthMode=OPEN/AuthMode=WPA2PSK/g' package/emortal/mt-drivers/mt_wifi/files/mt7615.5G.dat
+sed -i '1,/EncrypType=NONE/s/EncrypType=NONE/EncrypType=AES/' package/emortal/mt-drivers/mt_wifi/files/mt7615.5G.dat
+sed -i 's/WPAPSK1=12345678/WPAPSK1=987654321/g' package/emortal/mt-drivers/mt_wifi/files/mt7615.5G.dat
 
 #2g
 sed -i 's/Channel=6/Channel=11/g' package/emortal/mt-drivers/mt_wifi/files/mt7603.dat
