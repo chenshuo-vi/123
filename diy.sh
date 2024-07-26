c916539364427b27c33b479edea312a1f8ee66f9
chenshuo-vi <84225266+chenshuo-vi@users.noreply.github.com>
2024-07-27 01:57:04 +0800

    Update diy.sh

--- diy.sh
+++ diy.sh
@@ -2,13 +2,14 @@
 sed -i 's/192.168.1.1/192.168.2.1/g' package/base-files/files/bin/config_generate
 
 #下载主题应用
-rm -rf feeds/luci/themes/luci-theme-argon
-git clone -b 18.06 https://github.com/chenshuo-l/luci-theme-argon.git package/luci-theme-argon
-#rm -rf feeds/luci/themes/luci-theme-design
-#git clone https://github.com/gngpp/luci-theme-design.git  package/luci-theme-design
+#rm -rf feeds/luci/themes/luci-theme-argon
+#git clone -b 18.06 https://github.com/chenshuo-l/luci-theme-argon.git package/luci-theme-argon
+rm -rf feeds/luci/themes/luci-theme-design
+git clone https://github.com/gngpp/luci-theme-design.git  package/luci-theme-design
 
 #修改主题
-sed -i 's/luci-theme-bootstrap/luci-theme-argon/g' feeds/luci/collections/luci/Makefile
+#sed -i 's/luci-theme-bootstrap/luci-theme-argon/g' feeds/luci/collections/luci/Makefile
+sed -i 's/luci-theme-bootstrap/luci-theme-design/g' feeds/luci/collections/luci/Makefile
 
 #修改wifi信息
 #5g
